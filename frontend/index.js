586c055aa3b63cdb99efea9331ce2b3a5ca4fbe3
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-10-29 23:14:09 +0530

    update

--- frontend/index.js
+++ frontend/index.js
@@ -16,9 +16,9 @@ function Check(){
     
     const arr = [{ car: "Hyundai i20", price: 600000},
                  { car: "Toyota Corola", price: 900000}, 
-                 { car: "Scoda Rapid", price: 900000},
-                 { car: "Ford Endevour", price: 900000},
-                 { car: "Jeep Compas", price: 900000}
+                 { car: "Scoda Rapid", price: 1100000},
+                 { car: "Ford Endevour", price: 2100000},
+                 { car: "Jeep Compas", price: 1600000}
                 ];
 
     let op=arr.filter((a)=>a.car===CarModel);
@@ -29,14 +29,23 @@ function Check(){
     if(CarYear==2013){
         price-=price*0.12;
     }
-    if(CarKm==2000){
-        price-=price*0.07;
+    if(CarYear==2010){
+        price-=price*0.13;
     }
     if(CarKm==5000){
-        price-=price*0.1;
+        price-=price*0.04;
+    }
+    if(CarKm==10000){
+        price-=price*0.06;
+    }
+    if(CarKm==20000){
+        price-=price*0.08;
+    }
+    if(CarKm==30000){
+        price-=price*0.9;
     }
     if(CarInsurance=="no"){
-        price-=price*0.1;
+        price-=price*0.4;
     }    
     if(CarWoner==2){
         price-=price*0.2;
