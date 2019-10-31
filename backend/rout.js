92039640650c0ffa66d28a138ec636db36385d68
satyajitdash61 <32846765+satyajitdash61@users.noreply.github.com>
2019-10-31 17:46:45 +0530

    update

--- backend/rout.js
+++ backend/rout.js
@@ -39,13 +39,42 @@ const Upload =require("./Model/Uploading");
           });          
     });
     router.get("/calculation/:b",(req,res) =>{
-      let p;
+      
        let {car,Car_Year,Car_km,Car_Insurance,Car_Owner} = JSON.parse(req.params.b);
        Car.findOne({car})
        .then(car =>{
-        p=JSON.parse(JSON.stringify(car)).price;
+        let Car_Price=JSON.parse(JSON.stringify(car)).price;
+        console.log(Car_Price);
+        
+        if(CarYear==2015){
+        price-=price*0.1;
+    }
+    if(CarYear==2013){
+        price-=price*0.12;
+    }
+    if(CarYear==2010){
+        price-=price*0.13;
+    }
+    if(CarKm==5000){
+        price-=price*0.04;
+    }
+    if(CarKm==10000){
+        price-=price*0.06;
+    }
+    if(CarKm==20000){
+        price-=price*0.08;
+    }
+    if(CarKm==30000){
+        price-=price*0.9;
+    }
+    if(CarInsurance=="no"){
+        price-=price*0.4;
+    }    
+    if(CarWoner==2){
+        price-=price*0.2;
+    }
+
        })
-      console.log(p);
       
     })
  
